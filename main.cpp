d0a6731172c9724253641a7f04a3014013d1e031
Nontawat Wuttikam <nontawatwuttikam@gmail.com>
2024-04-11 20:45:49 +0700

    Impl cast multiple rays within fov :D

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 
 bool isPress = false;
+bool isDebug = false;
 const double PI = 3.14159265359;
 float EPS = 0.00005;
 float F_INF = 1e+18;
@@ -24,6 +25,19 @@ void init() {
 
 template<typename T, typename... Args>
 void print(T firstArg, Args... args) {
+    if (isPress && isDebug) {
+        std::cout << firstArg; // Print the first argument
+
+        // Print the rest of the arguments space-separated
+        ((std::cout << ' ' << args), ...);
+        
+        std::cout << std::endl; // End the line
+    }
+}
+
+// print without debug condition
+template<typename T, typename... Args>
+void print_(T firstArg, Args... args) {
     if (isPress) {
         std::cout << firstArg; // Print the first argument
 
@@ -51,11 +65,13 @@ int main(int argc, char** argv) {
     glutMainLoop();
 }
 
+int numRays = 30;
+int fov = 60.0f;
 int offsetX = -10;
 int offsetY = -10;
 float playerX = 1.45f;
 float playerY = 2.37f;
-double playerAngle = 45.0;
+float playerAngle = 45.0f;
 double rays_t[] = {0}; // rays length, for simplicity lets cast 1 ray so the we don't fuck up
 const int worldSize = 20;
 const int worldMap[worldSize][worldSize] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -253,136 +269,152 @@ void castRays() {
     
     //DDA Raycaster naive, verbose, highly unoptimized, implementation
 
-    // transform player from viewport to world coord
-    float pX = playerX - offsetX;
-    float pY = worldSize - (playerY - offsetY);
-    print("pX,pY",pX,pY);
-    
-    // check quadrant of current player angle
-    int quadrant = isAtQuadrant(playerAngle);
-    float deg = playerAngle;
-    double lx = 0; // ray length (hypotenuse) wrt lx 
-    double ly = 0; // ray length (hypotenuse) wrt ly
-    float xlen = 0; // length in x axis to step +1
-    float ylen = 0; // length in y axis to step + 1
+    // cast rays within range of fov
+    float half_fov = fov/2.0f;
+    float start_angle = ((playerAngle - half_fov) >= 0.0f)? (playerAngle - half_fov) : (playerAngle - half_fov) + 360.0f;
+    float end_angle = ((playerAngle + half_fov) < 360.0f)? (playerAngle + half_fov) : (playerAngle + half_fov) - 360.0f;
+    float current_angle = start_angle;
 
-    float ax = floor(pX); // grid origin that player is currently in.
-    float ay = floor(pY);
-    float vx = 1.0f; // direction to step lx 
-    float vy = 1.0f; // direction to step ly
-    // cout << "quadrant " << quadrant << "\n";
-    switch(quadrant) {
-        case 0: xlen = 1.0f - (pX - ax);
-                ylen = (pY - ay); 
-                break;
-        case 1: deg = 180 - playerAngle;
-                xlen = (pX - ax);
-                ylen = (pY - ay); 
-                vx = -1.0f;
-                break;
-        case 2: deg = playerAngle - 180;
-                xlen = (pX - ax);
-                ylen = 1.0f - (pY - ay); 
-                vx = -1.0f;
-                vy = -1.0f;
-                break;
-        case 3: deg = 360 - playerAngle;
-                xlen = 1.0f - (pX - ax);
-                ylen = 1.0f - (pY - ay); 
-                vy = -1.0f;
-                break;
-    }
-    if (fabs(deg - 90) < EPS) lx = F_INF; // prevent numerical issues when divide by cos90 = 0 must be inf.
-    else lx = xlen/cos((deg*PI)/180.f);
-    ly = ylen/sin((deg*PI)/180.f);
+    // step size for angle
+    float step_size = fov/((float) numRays);
 
-    print("AxAy",ax,ay);
-    print("playerAngle",playerAngle);
-    print("deg",deg);
-    print("xlen,ylen",xlen,ylen);
-    print("lx,ly",lx,ly);
-    print("cos,sin ",std::cos((deg*PI)/180.f),std::sin((deg*PI)/180.f));
-
-    float* endOfRaysLx = findEndOfRay(pX, pY, lx, playerAngle); // find initial end of ray given length lx 
-    float* endOfRaysLy = findEndOfRay(pX, pY, ly, playerAngle); // find initial end of ray given length ly
-    float finalLx = F_INF; // set final ray length to inf
-    float finalLy = F_INF; // set final ray length to inf
-    bool isLxHit = false; // flag to check if rayLx is hit something
-    bool isLyHit = false; // flag to check if rayLy is hit something
-    int maxStep = worldSize*2; //maximum step to prvent forever ray cast (edge case, bugs)
-    int count = 0;
-
-    while (true) { // tip of the ray is in wall
-        if (hitSomething(endOfRaysLx[0], endOfRaysLx[1]) && !isLxHit) {finalLx = lx; isLxHit = true; print(">Red Hit at", endOfRaysLx[0], endOfRaysLx[1]);} // check if RayLx hit any facet of the wall
-        if (hitSomething(endOfRaysLy[0], endOfRaysLy[1]) && !isLyHit) {finalLy = ly; isLyHit = true; print(">Green Hit at", endOfRaysLy[0], endOfRaysLy[1]);} // check if RayLy hit any facet of the wall
-        if (isLxHit && isLyHit) break; // if both rays hit, stop
-
-        // if not, increment short ray along x,y axis by 1
-        if (lx <= ly) {
-            xlen += 1.0f;
-            lx = xlen/cos((deg*PI)/180.f); // compute the ray len again
-        }
-        else if (ly < lx) {
-            ylen += 1.0f;
-            ly = ylen/sin((deg*PI)/180.0); // compute the ray len again
-        }
-
-        endOfRaysLx = findEndOfRay(pX, pY, lx, playerAngle); // find the end of ray given origin and ray length Lx
-        endOfRaysLy = findEndOfRay(pX, pY, ly, playerAngle); // find the end of ray given origin and ray length Ly
-        count++;
-
-        print("Red",count,endOfRaysLx[0],endOfRaysLx[1]);
-        print("Green",count,endOfRaysLy[0],endOfRaysLy[1]);
+    print_("player,start,end angle", playerAngle, start_angle, end_angle);
+    while(fabs(current_angle - end_angle) > EPS) {
+        // transform player from viewport to world coord
+        float pX = playerX - offsetX;
+        float pY = worldSize - (playerY - offsetY);
+        print("pX,pY",pX,pY);
         
-        // draw a point on a grid line in both x and y axis which a ray at any step stop at.
-        drawPoint(endOfRaysLx[0] + offsetX, -1*(endOfRaysLx[1] + offsetY), 1.0, 0.0, 0.0);
-        drawPoint(endOfRaysLy[0] + offsetX, -1*(endOfRaysLy[1] + offsetY), 0.0, 1.0, 0.0);
+        // check quadrant of current player angle
+        int quadrant = isAtQuadrant(current_angle);
+        float deg = current_angle;
+        double lx = 0; // ray length (hypotenuse) wrt lx 
+        double ly = 0; // ray length (hypotenuse) wrt ly
+        float xlen = 0; // length in x axis to step +1
+        float ylen = 0; // length in y axis to step + 1
 
-        // check if the endOfRayLx is outside the map, we won't step further
-        if (endOfRaysLx[0] < 0 || endOfRaysLx[0] > worldSize || endOfRaysLx[1] < 0 || endOfRaysLx[1] > worldSize) { // both endofrayLx and endofrayLy are out of bound
-            print("Red invalid");
-            // break;
-            isLxHit = true; // we won't step further
+        float ax = floor(pX); // grid origin that player is currently in.
+        float ay = floor(pY);
+        float vx = 1.0f; // direction to step lx 
+        float vy = 1.0f; // direction to step ly
+        // cout << "quadrant " << quadrant << "\n";
+        switch(quadrant) {
+            case 0: xlen = 1.0f - (pX - ax);
+                    ylen = (pY - ay); 
+                    break;
+            case 1: deg = 180 - current_angle;
+                    xlen = (pX - ax);
+                    ylen = (pY - ay); 
+                    vx = -1.0f;
+                    break;
+            case 2: deg = current_angle - 180;
+                    xlen = (pX - ax);
+                    ylen = 1.0f - (pY - ay); 
+                    vx = -1.0f;
+                    vy = -1.0f;
+                    break;
+            case 3: deg = 360 - current_angle;
+                    xlen = 1.0f - (pX - ax);
+                    ylen = 1.0f - (pY - ay); 
+                    vy = -1.0f;
+                    break;
         }
-        // check if the endOfRayLx is outside the map, we won't step further
-        if (endOfRaysLy[0] < 0 || endOfRaysLy[0] > worldSize || endOfRaysLy[1] < 0 || endOfRaysLy[1] > worldSize) { // both endofrayLx and endofrayLy are out of bound
-            print("Green invalid");
-            // break;
-            isLyHit = true; // we won't step further
+        if (fabs(deg - 90) < EPS) lx = F_INF; // prevent numerical issues when divide by cos90 = 0 must be inf.
+        else lx = xlen/cos((deg*PI)/180.f);
+        ly = ylen/sin((deg*PI)/180.f);
+
+        print("AxAy",ax,ay);
+        print("playerAngle",current_angle);
+        print("deg",deg);
+        print("xlen,ylen",xlen,ylen);
+        print("lx,ly",lx,ly);
+        print("cos,sin ",std::cos((deg*PI)/180.f),std::sin((deg*PI)/180.f));
+
+        float* endOfRaysLx = findEndOfRay(pX, pY, lx, current_angle); // find initial end of ray given length lx 
+        float* endOfRaysLy = findEndOfRay(pX, pY, ly, current_angle); // find initial end of ray given length ly
+        float finalLx = F_INF; // set final ray length to inf
+        float finalLy = F_INF; // set final ray length to inf
+        bool isLxHit = false; // flag to check if rayLx is hit something
+        bool isLyHit = false; // flag to check if rayLy is hit something
+        int maxStep = worldSize*2; //maximum step to prvent forever ray cast (edge case, bugs)
+        int count = 0;
+
+        while (true) { // tip of the ray is in wall
+            if (hitSomething(endOfRaysLx[0], endOfRaysLx[1]) && !isLxHit) {finalLx = lx; isLxHit = true; print(">Red Hit at", endOfRaysLx[0], endOfRaysLx[1]);} // check if RayLx hit any facet of the wall
+            if (hitSomething(endOfRaysLy[0], endOfRaysLy[1]) && !isLyHit) {finalLy = ly; isLyHit = true; print(">Green Hit at", endOfRaysLy[0], endOfRaysLy[1]);} // check if RayLy hit any facet of the wall
+            if (isLxHit && isLyHit) break; // if both rays hit, stop
+
+            // if not, increment short ray along x,y axis by 1
+            if (lx <= ly) {
+                xlen += 1.0f;
+                lx = xlen/cos((deg*PI)/180.f); // compute the ray len again
+            }
+            else if (ly < lx) {
+                ylen += 1.0f;
+                ly = ylen/sin((deg*PI)/180.0); // compute the ray len again
+            }
+
+            endOfRaysLx = findEndOfRay(pX, pY, lx, current_angle); // find the end of ray given origin and ray length Lx
+            endOfRaysLy = findEndOfRay(pX, pY, ly, current_angle); // find the end of ray given origin and ray length Ly
+            count++;
+
+            print("Red",count,endOfRaysLx[0],endOfRaysLx[1]);
+            print("Green",count,endOfRaysLy[0],endOfRaysLy[1]);
+            
+            // draw a point on a grid line in both x and y axis which a ray at any step stop at.
+            drawPoint(endOfRaysLx[0] + offsetX, -1*(endOfRaysLx[1] + offsetY), 1.0, 0.0, 0.0);
+            drawPoint(endOfRaysLy[0] + offsetX, -1*(endOfRaysLy[1] + offsetY), 0.0, 1.0, 0.0);
+
+            // check if the endOfRayLx is outside the map, we won't step further
+            if (endOfRaysLx[0] < 0 || endOfRaysLx[0] > worldSize || endOfRaysLx[1] < 0 || endOfRaysLx[1] > worldSize) { // both endofrayLx and endofrayLy are out of bound
+                print("Red invalid");
+                // break;
+                isLxHit = true; // we won't step further
+            }
+            // check if the endOfRayLx is outside the map, we won't step further
+            if (endOfRaysLy[0] < 0 || endOfRaysLy[0] > worldSize || endOfRaysLy[1] < 0 || endOfRaysLy[1] > worldSize) { // both endofrayLx and endofrayLy are out of bound
+                print("Green invalid");
+                // break;
+                isLyHit = true; // we won't step further
+            }
+
+            // if weird case happened (I am not John carmack bro :/) stop at maxStep. it is better than stucking in infinite loop.
+            count++;
+            if (count == maxStep) break;
         }
 
-        // if weird case happened (I am not John carmack bro :/) stop at maxStep. it is better than stucking in infinite loop.
-        count++;
-        if (count == maxStep) break;
-    }
+        float finalL = min(finalLx, finalLy);
 
-    float finalL = min(finalLx, finalLy);
+        print("FinalendOfRaysLx", endOfRaysLx[0], endOfRaysLx[1]);
+        print("FinalendOfRaysLy", endOfRaysLy[0], endOfRaysLy[1]);
 
-    print("FinalendOfRaysLx", endOfRaysLx[0], endOfRaysLx[1]);
-    print("FinalendOfRaysLy", endOfRaysLy[0], endOfRaysLy[1]);
-
-    print("xlen,ylen",xlen,ylen);
-    print("LxLy",lx,ly);
-    print("finalLxLy",finalLx, finalLy);
-    print("finalL",finalL);
+        print("xlen,ylen",xlen,ylen);
+        print("LxLy",lx,ly);
+        print("finalLxLy",finalLx, finalLy);
+        print("finalL",finalL);
 
 
-    //draw ray
-    glLoadIdentity();
-    glTranslatef(playerX, playerY, 0.0f);
-    glRotatef(playerAngle, 0.0f, 0.0f, 1.0f);
-    glBegin(GL_LINES);
-    glColor3f(1.0, 1.0, 0.0);
-    glVertex2f(0,0);
-    double uVecX = cos((deg*PI)/180.f);
-    double uVecY = sin((deg*PI)/180.f);
-    glVertex2f(finalL, 0);
-    glColor3f(1.0, 1.0, 1.0);
-    glEnd();
+        //draw ray
+        glLoadIdentity();
+        glTranslatef(playerX, playerY, 0.0f);
+        glRotatef(current_angle, 0.0f, 0.0f, 1.0f);
+        glBegin(GL_LINES);
+        glColor3f(1.0, 1.0, 0.0);
+        glVertex2f(0,0);
+        double uVecX = cos((deg*PI)/180.f);
+        double uVecY = sin((deg*PI)/180.f);
+        glVertex2f(finalL, 0);
+        glColor3f(1.0, 1.0, 1.0);
+        glEnd();
+        print("\n");
 
-    print("\n");
-
-    
+        // step angle
+        current_angle += step_size;
+        if (current_angle >= 360.0f) {
+            current_angle = current_angle - 360.0f;
+            print_("do");
+        }
+    }    
 }
 
 void displayCallback() {
